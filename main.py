b972e86fc4b217ec9f9f26129cafabe7cb1b4081
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2023-11-17 16:25:06 -0300

    feat: enhance planet gravity

--- main.py
+++ main.py
@@ -1,48 +1,34 @@
 import pygame
-from bullet import Bullet
 from planet import Planet
-from math import atan2, sin, cos, sqrt
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
-time_span = 1
 
 middle_width = screen.get_width()/2
 middle_height = screen.get_height()/2
 
-planet_radius = 100
-bullet_radius = 15
+planet_radius = 80
+bullet_radius = 5
 
-initial_force = pygame.Vector2(1.10, 0)
+bullet = Planet(middle_width, middle_height - planet_radius - 40, bullet_radius, 1, pygame.Vector2(4, 0), "blue")
 
-def calculate_gravity(bullet, planet):
-  distance_vector = planet.position - bullet.position
-  dx, dy = distance_vector.x, distance_vector.y
-  d = sqrt(dx ** 2 + dy ** 2)
-  F = bullet.mass * planet.mass / pow(d, 2)
-  a = F / bullet.mass
-  v = a * time_span
+planet = Planet(middle_width, middle_height, planet_radius, 1700, pygame.Vector2(0, 0), "red")
 
-  theta = atan2(dy, dx)
-
-  v_vector = pygame.Vector2(v * cos(theta), v * sin(theta))
-  return v_vector
-
-bullet = Bullet(middle_width, middle_height - planet_radius - 50, bullet_radius, initial_force)
-
-planet = Planet(middle_width, middle_height, planet_radius)
+planets = [bullet, planet]
 
 while running:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         running = False
-
-    bullet.move(calculate_gravity(bullet, planet))
+       
     screen.fill("black")
-    bullet.render(screen)
-    planet.render(screen)
+
+    for planet in planets:
+      planet.calculate_gravity(planets)
+      planet.render(screen)
+    
     pygame.display.flip()
 
     clock.tick(60)
--- planet.py
+++ planet.py
@@ -1,10 +1,33 @@
 import pygame
+from math import atan2, sqrt, cos, sin
 
 class Planet:
-  def __init__(self, x, y, radius):
+  def __init__(self, x, y, radius, mass, initialVelocity, color):
     self.position = pygame.Vector2(x, y)
     self.radius = radius
-    self.mass = 200
+    self.mass = mass
+    self.velocity = initialVelocity
+    self.color = color
 
   def render(self, screen):
-    pygame.draw.circle(screen, "blue", self.position, self.radius)
+    self.position += self.velocity
+    pygame.draw.circle(screen, self.color, self.position, self.radius)
+
+  def calculate_gravity(self, planets):
+    total_vector = pygame.Vector2(0, 0)
+
+    for planet in planets:
+      if planet == self:
+        continue
+
+      distance_vector = planet.position - self.position
+      dx, dy = distance_vector.x, distance_vector.y
+      d = sqrt(dx ** 2 + dy ** 2)
+      F = self.mass * planet.mass / pow(d, 2)
+      a = F / self.mass
+      v = a
+
+      theta = atan2(dy, dx)
+      total_vector += pygame.Vector2(v * cos(theta), v * sin(theta))
+
+    self.velocity += total_vector
